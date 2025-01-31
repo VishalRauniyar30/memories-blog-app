f3a7708ad847dc42b06efbcdb3b4eb02a059fdc9
Niket Rauniyar <vishal@gmail.com>
2025-01-31 18:31:07 +0530

    small change in api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000' });
-// const API = axios.create({ baseURL: 'https://memories-blog-app-server.vercel.app/' });
+// const API = axios.create({ baseURL: 'http://localhost:8000' });
+const API = axios.create({ baseURL: 'https://memories-blog-app-server.vercel.app/' });
 
 
 API.interceptors.request.use((req) => {
